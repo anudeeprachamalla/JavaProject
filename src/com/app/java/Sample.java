039d1e83ede804e9177c2a8cc19b9acab4a952c3
Dell <Dell@Dell-PC>
2019-04-16 16:50:38 +0530

    commit

--- src/com/app/java/Sample.java
+++ src/com/app/java/Sample.java
@@ -5,7 +5,10 @@ public class Sample {
 	void m1() {
 		System.out.println("vaagdevi engineering college");
 	}
-
+void m()
+{
+	System.out.println("iiiiiii");
+}
 	public static void main(String[] args) {
 
 		Sample s = new Sample();
