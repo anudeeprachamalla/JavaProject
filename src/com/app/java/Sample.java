97b0e7bf4648fe820baffa094d5ebbd4ab85f9d5
Dell <Dell@Dell-PC>
2019-04-16 16:36:24 +0530

    local

--- src/com/app/java/Sample.java
+++ src/com/app/java/Sample.java
@@ -6,24 +6,9 @@ public class Sample {
 		System.out.println("vaagdevi engineering college");
 	}
 
-	void m2() {
-		m1();
-		System.out.println("jayamukhi engineering college");
-	}
-
-	void m3() {
-		System.out.println("ganapathy engineering college");
-		System.out.println("shivani engineering college");
-	}
-
-	void m4() {
-		m3();
-		System.out.println("sr engineering college");
-	}
-
 	public static void main(String[] args) {
 
 		Sample s = new Sample();
-		s.m2();
+
 	}
 }
